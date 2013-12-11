a1d59f5d3c47f6dcf5955090930bc53cd72d9423
ronniedada <ronniedada@gmail.com>
2013-12-10 17:25:10 -0800

    Heal the client partition after 30 seconds

--- salticid/couchbase/partition_client.sh
+++ salticid/couchbase/partition_client.sh
@@ -1,4 +1,12 @@
 #!/bin/bash 
+
+echo "Drop connections to n3, n4, n5"
 iptables -A INPUT -s n3 -j DROP
 iptables -A INPUT -s n4 -j DROP
 iptables -A INPUT -s n5 -j DROP
+
+echo "Sleep for 30 seconds"
+sleep 30
+
+echo "Reconnect to n3, n4, n5"
+iptables -F
