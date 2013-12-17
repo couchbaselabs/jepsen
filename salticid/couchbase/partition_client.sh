4ec2a8542085dbb6509a96db71c667322aa8c86d
ronniedada <ronniedada@gmail.com>
2013-12-17 10:55:59 -0800

    Saparate client from active partition

--- salticid/couchbase/partition_client.sh
+++ salticid/couchbase/partition_client.sh
@@ -1,12 +1,11 @@
 #!/bin/bash 
 
-echo "Drop connections to n3, n4, n5"
-iptables -A INPUT -s n3 -j DROP
-iptables -A INPUT -s n4 -j DROP
-iptables -A INPUT -s n5 -j DROP
+echo "Drop connections to n1, n2"
+iptables -A INPUT -s n1 -j DROP
+iptables -A INPUT -s n2 -j DROP
 
 echo "Sleep for 30 seconds"
 sleep 30
 
-echo "Reconnect to n3, n4, n5"
+echo "Reconnect to n1, n2"
 iptables -F
